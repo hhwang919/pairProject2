bff90e9a53d96f1e6bfb7ab67917f8a4f7342a6e
Hubert Hwang <huberthwang919@gmail.com>
2022-10-06 15:45:21 -0700

    16-coupon

--- problems/16-coupon.js
+++ problems/16-coupon.js
@@ -16,7 +16,18 @@ console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 ***********************************************************************/
 
 // Your code here
+let coupon = function(discount){
+  return (prices) =>{
+  return prices.map((el) => {el -= el * discount; return el})
+  }
+}
 
+let tenPercent = coupon(0.1);
+console.log(tenPercent([10, 20, 30])); // [ 9, 18, 27 ]
+
+
+let twentyPercent = coupon(0.2);
+console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
